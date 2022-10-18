21c9a5704cffc39be325c666d4a4a022e006d37b
Richard <charichard09@gmail.com>
2022-10-18 12:11:47 -0700

    add checking if 'f' or 'c' was entered, if not, print error
    Co-authored-by: Auston Moos <Austonhmoos2004@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,17 @@
 function tempConvert() {
   const number1 = window.prompt("Please enter a number in degrees:");
-  const tempChar = window.prompt("Please enter a C or F for temp"); 
+  const tempChar = window.prompt("Will we be converting from C or F for temp"); 
   let temp = 0;
-  
-  if (tempChar === "C") {
-    temp = (number1 - 32) * (5/9); 
+
+  if (tempChar.toUpperCase() === "C") {
+    temp = (number1 * 9/5) + 32; 
+  } else if (tempChar.toUpperCase() === "F") {
+    temp = (number1 - 32) * (5/9);
   } else {
-    temp = number1 * (9/5) + 32;
+    return "ERROR: not an F or a C";
   }
 
-  return temp
+  return temp;
 }
 
 window.alert(tempConvert());
